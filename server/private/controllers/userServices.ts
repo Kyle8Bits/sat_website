f10a0b6d4b84095dc5d68a53785d72082019dff9
Milkyway Galaxy <blannkisblank@gmail.com>
2025-06-08 23:19:39 +0700

    Add: moved services to controllers, add more feats

---
+++ server/private/controllers/userServices.ts
@@ -0,0 +1,62 @@
+import User, { IUser } from '../models/User';
+import bcrypt from 'bcrypt';
+
+export const createUser = async (data: IUser): Promise<void> => {
+    const user = new User(data);
+    user.save();
+};
+
+export const deleteUser = async (userId: string): Promise<boolean> => {
+    const existingUser = await User.findById(userId);
+
+    if (!existingUser) {
+        return false;
+    }
+
+    await User.deleteOne({ _id: userId });
+
+    return true;
+};
+
+export const changePassword = async (
+    userId: string,
+    oldPassword: string,
+    newPassword: string
+): Promise<boolean> => {
+    const existingUser = await User.findById(userId);
+
+    if (!existingUser) {
+        return false;
+    }
+
+    const isMatch = await bcrypt.compare(oldPassword, existingUser.password);
+    if (!isMatch) {
+        return false; // old password incorrect
+    }
+
+    existingUser.password = newPassword; // No need to hash because hashing is handled by User Object (for some reason)
+    await existingUser.save();
+
+    return true;
+};
+
+export const changeNick = async (
+    userId: string,
+    oldNick: string,
+    newNick: string
+): Promise<boolean> => {
+    const existingUser = await User.findById(userId);
+
+    if (!existingUser) {
+        return false;
+    }
+
+    if (oldNick === newNick) {
+        return false; // same name
+    }
+
+    existingUser.nickname = newNick;
+    await existingUser.save();
+
+    return true;
+};
