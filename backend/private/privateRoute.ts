26d4e1bfe23308abd67f89347ae058f69b41ff2d
Milkyway Galaxy <blannkisblank@gmail.com>
2025-06-13 23:40:41 +0700

    Update privateRoute.ts

--- backend/private/privateRoute.ts
+++ backend/private/privateRoute.ts
@@ -16,6 +16,18 @@ router.get('/user', (req, res) => {
     }
 });
 
+router.get('/dashboard', authenticateToken, (req, res) => {
+    try {
+        res.status(200).json({
+            message: 'User Kyle'
+        });
+    }
+    catch (error) {
+        console.error('Error in public route:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 router.post('/login', loginUser);
 router.post('/register', checkUsedEmail, registerUser);
 router.post('/checkemail', checkAllowedEmail);
